2aa3d930f416a95290d6fa2b9c457a8d46e661dd
Hussain Hassam <hussain.hassam21@gmail.com>
2015-09-25 16:00:03 -0700

    minor fully moving with proper animations

--- The Neutral/Assets/Scripts/Animations/MinorAnimations.cs
+++ The Neutral/Assets/Scripts/Animations/MinorAnimations.cs
@@ -3,55 +3,59 @@ using System.Collections;
 
 public class MinorAnimations : MonoBehaviour {
 
-	public Animator anim;
-	public CharacterController cont;
-	// Use this for initialization
+    public int speed_const;
+	private Animator anim;
+	private CharacterController cont;
+
+
 	int isDead = Animator.StringToHash("isDead");
-	int isRunning = Animator.StringToHash("isRunning");
 	int isMerge = Animator.StringToHash("isMerge");
 	int isRespawn = Animator.StringToHash("isRespawn");
+    int speed = Animator.StringToHash("speed");
 	private Vector3 moveDirection = Vector3.zero;
 
 	void Start () {
+
 		anim = GetComponent<Animator>();	
 		cont = GetComponent<CharacterController> ();
-	}
+    }
 	
-	// Update is called once per frame
+
 	void Update () 
 	{
 		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 		moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
 		moveDirection = transform.TransformDirection(moveDirection);
+        anim.SetFloat(speed, Input.GetAxis("Vertical"));
+        if (Mathf.Abs(moveDirection.z) > 0)
+        {
+            anim.Play("Run");
+        }
+        else if (stateInfo.IsName("Run")) {
+            Debug.Log("Setting animation to idle");
+            anim.Play("Idle");
+        }
 
-		if (Mathf.Abs(moveDirection.z) > 0) {
-			anim.SetBool (isRunning, true);
-			anim.Play ("Run");
-		} else
-			anim.SetBool (isRunning, false);
+        if (stateInfo.IsName("Run"))
+        {
+            cont.Move(moveDirection * Time.deltaTime * speed_const);
+            transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0));
+        }
 
-		if (stateInfo.IsName("Run")) {
-
-			cont.Move(moveDirection*Time.deltaTime*8);
-			transform.Rotate (new Vector3(0,Input.GetAxis("Horizontal"),0));
-
-
-		}
-		if (Input.GetKey ("p")) {
+		if (Input.GetKey ("z")) {
 			anim.SetBool(isDead, true);
 			anim.Play ("Death");
 		}
-		else if (Input.GetKey("m")) { 
+		else if (Input.GetKey("c")) { 
 			anim.SetBool(isMerge, true); 
 			anim.Play ("Merge");
 		}
-		else if (Input.GetKey("k")) { 
+		else if (Input.GetKey("x")) { 
 			anim.SetBool(isRespawn, true); 
 			anim.Play("Respawn");
 		}
 	
-	if (Input.GetKey ("r")) {
-			anim.SetBool (isRunning, false);
+	    if (Input.GetKey ("r")) {
 			anim.SetBool (isDead, false);
 			anim.SetBool (isMerge, false);
 			anim.SetBool (isRespawn, false);
---
+++ The Neutral/Assets/Scripts/ModifyMaterial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModifyMaterial : MonoBehaviour {
+
+    private Renderer ren;
+    public Material[] materials;
+    public int i;
+	// Use this for initialization
+	void Start () {
+        ren = GetComponent<Renderer>();
+        materials = Resources.LoadAll<Material>("Materials");
+        Debug.Log("We have "+materials.Length+" materials loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp("i"))
+        {
+            ren.material = materials[i%materials.Length];
+            i += 1;
+        }
+
+	}
+}
