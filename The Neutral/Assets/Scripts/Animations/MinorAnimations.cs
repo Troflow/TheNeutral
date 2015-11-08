e6db8350037ef87276a4d76adf34cffa54f94348
Hussain <hussain.hassam21@gmail.com>
2015-11-08 15:59:27 -0800

    dash implemented

--- The Neutral/Assets/Scripts/Animations/MinorAnimations.cs
+++ The Neutral/Assets/Scripts/Animations/MinorAnimations.cs
@@ -5,8 +5,11 @@ public class MinorAnimations : MonoBehaviour {
 
     public int speed_const;
 	private Animator anim;
+	private Animation animationList;
 	private CharacterController cont;
-
+	private bool isShiftDown; //for setting the timer to start only during the first frame of when shift is hit
+	private float slowMoTime; //the amount of time you were in "slow motion" (charging up for the dash)
+	public AIPath currentMovement;
 
 	int isDead = Animator.StringToHash("isDead");
 	int isMerge = Animator.StringToHash("isMerge");
@@ -16,8 +19,12 @@ public class MinorAnimations : MonoBehaviour {
 
 	void Start () {
 
-		anim = GetComponent<Animator>();	
+		anim = GetComponent<Animator>();
+		animationList = GetComponent<Animation> ();
 		cont = GetComponent<CharacterController> ();
+		currentMovement = GetComponent<AIPath>();
+		isShiftDown = false;
+		slowMoTime = 0f;
     }
 	
 
@@ -29,11 +36,12 @@ public class MinorAnimations : MonoBehaviour {
 		anim.SetFloat(speed, Mathf.Abs (cont.velocity.magnitude));
 		if (Mathf.Abs(cont.velocity.z) > 0 || Mathf.Abs(cont.velocity.x) > 0)
         {
-            anim.Play("Run");
+
+			anim.Play("Run",0);
         }
         else if (stateInfo.IsName("Run")) {
             Debug.Log("Setting animation to idle");
-            anim.Play("Idle");
+            anim.Play("Idle",0);
         }
 
 		/*
@@ -43,6 +51,32 @@ public class MinorAnimations : MonoBehaviour {
             //transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0));
         }
 */
+
+		if(stateInfo.IsName("Run")) {
+			if (Input.GetKeyDown(KeyCode.LeftShift)) {
+				if (!isShiftDown) {
+					slowMoTime = Time.time;
+					anim.SetLayerWeight(1,1f);				
+				}
+				isShiftDown = true;
+
+				//animationList["Run"].speed = 0.5f;
+				currentMovement.speed = 2f;
+			}
+			if (Input.GetKeyUp(KeyCode.LeftShift)) {
+				if (isShiftDown) {
+					isShiftDown = false;
+					Debug.Log ("Time slow mo'd for: " + (Time.time - slowMoTime));
+					currentMovement.speed = 3f;
+					anim.SetLayerWeight(1,0f);
+				}
+
+				
+			}
+		}
+
+
+
 		if (Input.GetKey ("z")) {
 			anim.SetBool(isDead, true);
 			anim.Play ("Death");
@@ -56,6 +90,7 @@ public class MinorAnimations : MonoBehaviour {
 			anim.Play("Respawn");
 		}
 	
+	
 	    if (Input.GetKey ("r")) {
 			anim.SetBool (isDead, false);
 			anim.SetBool (isMerge, false);
@@ -64,4 +99,5 @@ public class MinorAnimations : MonoBehaviour {
 
 
 	}
+	
 }
